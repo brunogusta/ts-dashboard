45721e26645a7ead975723d01d4b98cab2ffdc43
Bruno Gustavo <brugucolosil95@gmail.com>
2021-11-07 10:57:00 -0300

    feat: add lib and default styles

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import GlobalStyles from "./styles/GlobalStyles";
+import Dashboard from "./pages/Dashboard";
+
+const App: React.FC = () => (
+  <>
+    <GlobalStyles />
+    <Dashboard />
+  </>
+);
+
+export default App;
